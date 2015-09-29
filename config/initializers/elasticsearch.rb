8e127bdf1d3818b4542f706cf59b5c7f77339a00
Steve Nolen <technolengy@gmail.com>
2015-09-29 10:09:02 -0700

    add support for es yml file to overwrite default settings

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,12 +1,21 @@
 # Be sure to restart your server when you modify this file.
 
 require 'elasticsearch'
+require 'yaml'
 
-Rails.application.config.elasticsearch_client = Elasticsearch::Client.new hosts: [
-  { host: 'localhost', port: 9200 }
-]
+config = {
+	hosts: [
+	  { host: 'localhost', port: 9200 }
+	],
+	index: 'apps'
+}
 
-Rails.application.config.elasticsearch_index = 'apps'
+if File.exists?("config/elasticsearch.yml")
+  config.merge!(YAML.load_file("config/elasticsearch.yml").deep_symbolize_keys)
+end
+
+Rails.application.config.elasticsearch_client = Elasticsearch::Client.new(config)
+Rails.application.config.elasticsearch_index = config[:index]
 
 begin
   Rails.application.config.elasticsearch_client.ping
