d890a4602c5538c62133ca372d5d11772c757222
Joshua Selsky <jselsky@oit.ucla.edu>
2015-10-09 10:17:03 -0700

    Fix comments.

--- config/initializers/casa.rb
+++ config/initializers/casa.rb
@@ -3,7 +3,10 @@ require 'yaml'
 # Community App Sharing Architecture configuration
 
 config = {
+    # This is the instance's originator_id. It should always be overridden using
+    # the YAML configuration.
     casa: { engine: { uuid: 'dd8c99e2-fe5b-4911-a815-73c17b46d3fc'} },
+    # Contact metadata for the storefront
     store: { user_contact: { name: 'John Doe', email: 'invalid@localhost'} }
 }
 
@@ -12,9 +15,6 @@ if File.exists?('config/casa.yml')
   Rails.application.config.merge!(YAML.load_file('config/casa.yml').deep_symbolize_keys)
 end
 
-
-# This is the instance's originator_id. It should always be overridden using
-# the YAML configuration.
 Rails.application.config.casa = config[:casa]
 
 Rails.application.config.store = config[:store]
