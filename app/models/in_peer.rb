f70406741f8467a8b3b5076c6ce99585e0703b2e
Joshua Selsky <jselsky@oit.ucla.edu>
2016-02-25 15:57:55 -0800

    Closes #95. In peers should be allowed to use HTTPS.

--- app/models/in_peer.rb
+++ app/models/in_peer.rb
@@ -2,36 +2,35 @@ require 'uri'
 require 'net/http'
 
 class InPeer < ActiveRecord::Base
-
   has_many :in_filter_rulesets
+  validates_with Validators::URIValidator
 
   def get_payloads
 
     uri = URI(self.uri)
     payloads = nil
 
-    Net::HTTP.start(uri.host, uri.port) do |http|
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true if uri.scheme == 'https'
 
-      request = Net::HTTP::Get.new uri
+    request = Net::HTTP::Get.new uri
 
-      request.add_field('Accept', 'application/json')
-      request.add_field('Accept-Charset', 'utf-8')
-      request.add_field('Content-Type', 'application/json')
+    request.add_field('Accept', 'application/json')
+    request.add_field('Accept-Charset', 'utf-8')
+    request.add_field('Content-Type', 'application/json')
 
-      data = {
-        'requestor_id' => Rails.application.config.casa[:engine][:uuid]
-      }
+    data = {
+      'requestor_id' => Rails.application.config.casa[:engine][:uuid]
+    }
 
-      if self.secret
-        data['secret'] = self.secret
-      end
-
-      request.body = data.to_json
-
-      response = http.request(request)
-      payloads = JSON.parse response.body
-
+    if self.secret
+      data['secret'] = self.secret
     end
+
+    request.body = data.to_json
+
+    response = http.request(request)
+    payloads = JSON.parse response.body
 
     payloads
 
