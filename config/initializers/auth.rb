a2c6962b9e6052e2fba4e7bc4e429012afdf84aa
Keith Rozett <krozett@gmail.com>
2021-02-19 00:11:27 -0800

    Quick fix for hosts that don't use keycloak

--- config/initializers/auth.rb
+++ config/initializers/auth.rb
@@ -6,13 +6,14 @@ Rails.application.config.login_route = '/session'
 
 # Include the auth configuration if it exists, otherwise the app will use the default login.
 if File.exists?('config/auth.yml')
-  OmniAuth.config.logger = Rails.logger
-
   config = YAML.load_file('config/auth.yml').deep_symbolize_keys
-
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider(*config[:omniauth])
-  end
-
   Rails.application.config.login_route = config[:login_route]
+
+  if config[:omniauth]
+    OmniAuth.config.logger = Rails.logger
+
+    Rails.application.config.middleware.use OmniAuth::Builder do
+      provider(*config[:omniauth])
+    end
+  end
 end
